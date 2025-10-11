5f8fcc58cde3ecf6e4f0fd74867a2a22b82ebd62
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-10-11 20:55:07 -0300

    fix: adiciona app.users.api e remove duplicado api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
 
     # Local
-    "api",
+    "app.api",
+    "app.users.api",
 
     # WhiteNoise helper p/ dev (opcional)
     "whitenoise.runserver_nostatic",
