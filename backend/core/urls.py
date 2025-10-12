4a0617c606ade78490eb556b5cd5ae72fc37ad1e
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-10-11 21:27:52 -0300

    fix: remove duplicate api app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),           
-    path("api/", include("users.api.urls")),     
+    path("api/", include("api.urls")),    
 ]
 
 if settings.DEBUG:
